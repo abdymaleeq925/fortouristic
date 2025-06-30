15c2bd180502aae6a90285977d386e0c9c3fa776
Abdymalik Batyrkulov <abdymalikbatyrkulov@gmail.com>
2025-06-30 13:44:27 +0300

    add: added MobileSidebar Navigation and 2 components(allUsers,dashboard)

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,17 @@
+import { Header } from 'components'
+import React from 'react'
+
+const AllUsers = () => {
+  return (
+    <main className='dashboard wrapper'>
+        <Header
+            title="Trips Page"
+            description="Check out our current users in real time."
+        />
+        AllUsers PageContents
+    </main>
+    
+  )
+}
+
+export default AllUsers
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,19 @@
+import { Header } from 'components';
+import React from 'react'
+
+const Dashboard = () => {
+    const user = {
+        name: "John Doe"
+    }
+  return (
+    <main className='dashboard wrapper'>
+        <Header
+            title={`Welcome ${user?.name || "Guest"}`}
+            description="Track activity, trends and popular destinations in real time."
+        />
+        Dashboard PageContents
+    </main>
+  )
+}
+
+export default Dashboard
